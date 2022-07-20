c623f5c66d2169941485737de67ba3afecc74e89
VasiliCheb <skimanvasia@gmail.com>
2022-07-21 00:13:23 +0300

    Создание разметки, заливка графики, фав-иконка, подготовка CSS

--- source/templates/data.js
+++ source/templates/data.js
@@ -1,7 +1,9 @@
 export default {
   "pages": [
     { "id": 0, "path": "index.html", "name": "Главная" },
-    { "id": 1, "path": "catalog.html", "name": "Каталог продукции" },
-    { "id": 2, "path": "form.html", "name": "Подбор программы" }
+    { "id": 1, "path": "advantages.html", "name": "Преимущества" },
+    { "id": 2, "path": "routes.html", "name": "Направления" },
+    { "id": 3, "path": "next-cruises.html", "name": "Ближайшие круизы" },
+    { "id": 4, "path": "contacts.html", "name": "Контакты" }
   ]
 }
