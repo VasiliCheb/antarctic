b7f12bf60c50ea765250e62e37882fd8a9c940fb
VasiliCheb <skimanvasia@gmail.com>
2022-07-23 00:40:27 +0300

    Доделал адаптивную верстку в  шапке и футере, оживил менюшку в мобилке JS, поставил фоновую картинку в Шапку + след. блок, приступил к адаптации след. блока.

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -1,1 +1,15 @@
-/*Код JS для переключателя меню*/
+const navMain = document.querySelector(".nav-menu");
+const navToggle = document.querySelector(".nav-menu__toggle");
+
+  navMain.classList.remove("nav-menu--nojs");
+  navMain.classList.add("nav-menu--closed");
+
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("nav-menu--closed")) {
+      navMain.classList.remove("nav-menu--closed");
+      navMain.classList.add("nav-menu--opened");
+    } else {
+      navMain.classList.add("nav-menu--closed");
+      navMain.classList.remove("nav-menu--opened");
+    }
+  });
--- source/templates/data.js
+++ source/templates/data.js
@@ -1,9 +1,8 @@
 export default {
   "pages": [
-    { "id": 0, "path": "index.html", "name": "Главная" },
-    { "id": 1, "path": "advantages.html", "name": "Преимущества" },
-    { "id": 2, "path": "routes.html", "name": "Направления" },
-    { "id": 3, "path": "next-cruises.html", "name": "Ближайшие круизы" },
-    { "id": 4, "path": "contacts.html", "name": "Контакты" }
+    { "id": 0, "path": "advantages.html", "name": "Преимущества" },
+    { "id": 1, "path": "routes.html", "name": "Направления" },
+    { "id": 2, "path": "next-cruises.html", "name": "Ближайшие круизы" },
+    { "id": 3, "path": "contacts.html", "name": "Контакты" }
   ]
 }
