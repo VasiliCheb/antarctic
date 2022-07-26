4d7d75f7bb5fd8b5d6824f811848d7eef8428714
VasiliCheb <skimanvasia@gmail.com>
2022-07-26 22:41:18 +0300

    Додеделал показ-шоу карточек Круизов. Добил JS в меню для смены логотипа при открытии в мобиле + дописал стили для лого

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -1,15 +1,19 @@
+const logoHeader = document.querySelector(".logo-header__img");
 const navMain = document.querySelector(".nav-menu");
-const navToggle = document.querySelector(".nav-menu__toggle");
+const navToggle = navMain.querySelector(".nav-menu__toggle");
 
   navMain.classList.remove("nav-menu--nojs");
   navMain.classList.add("nav-menu--closed");
+
 
   navToggle.addEventListener("click", function() {
     if (navMain.classList.contains("nav-menu--closed")) {
       navMain.classList.remove("nav-menu--closed");
       navMain.classList.add("nav-menu--opened");
+      logoHeader.classList.add("logo-header__img--opened");
     } else {
       navMain.classList.add("nav-menu--closed");
       navMain.classList.remove("nav-menu--opened");
+      logoHeader.classList.remove("logo-header__img--opened");
     }
   });
