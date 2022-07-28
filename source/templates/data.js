8104b2206f5c1f90c28048d6e52ba71057a6fa17
VasiliCheb <skimanvasia@gmail.com>
2022-07-29 00:02:06 +0300

    Прравки переполнения элементами

--- source/templates/data.js
+++ source/templates/data.js
@@ -1,8 +1,8 @@
 export default {
   "pages": [
-    { "id": 0, "path": "advantages.html", "name": "Преимущества" },
-    { "id": 1, "path": "routes.html", "name": "Направления" },
-    { "id": 2, "path": "next-cruises.html", "name": "Ближайшие круизы" },
-    { "id": 3, "path": "contacts.html", "name": "Контакты" }
+    { "id": 0, "path": "#advantages", "name": "Преимущества" },
+    { "id": 1, "path": "#", "name": "Направления" },
+    { "id": 2, "path": "#next-cruises", "name": "Ближайшие круизы" },
+    { "id": 3, "path": "#contacts", "name": "Контакты" }
   ]
 }
