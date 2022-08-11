1bc88fbd98f0d122f1223652b3cf525df59b7c74
VasiliCheb <skimanvasia@gmail.com>
2022-08-11 22:46:01 +0300

    Правки по багам тестировщика: Сделал логотип не кликабельным, Увеличил СЕО с 80 до 90%, реализовал Оверлей в мобильном меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 const body = document.querySelector('.page__body');
 const headerMenu = body.querySelector('.page-header__menu');
-const logoLink = headerMenu.querySelector('.page-header__logo-link');
 const navToggle = headerMenu.querySelector('.page-header__menu-toggle');
 const navLinkList = headerMenu.querySelectorAll('.page-header__menu-list a');
 
@@ -12,9 +11,16 @@ if (navToggle) {
     headerMenu.classList.toggle('page-header__menu--opened');
   });
 
+body.addEventListener('click',(evt) =>{
+  if (!headerMenu.contains(evt.target)) {
+    headerMenu.classList.remove('page-header__menu--opened');
+    body.classList.remove('page__body--noscroll');
+    }
+});
+
   navLinkList.forEach(item => {
     item.addEventListener('click', (evt) => {
-      if (headerMenu.classList.contains('page-header__menu--opened')) {
+      if (headerMenu.contains(evt.target)) {
         body.classList.remove('page__body--noscroll');
         headerMenu.classList.remove('page-header__menu--opened');
       }
